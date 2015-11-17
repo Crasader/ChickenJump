b77318d915a62ab3e323974a8b336ae6a6a4f7bc
asish012 <asish012@gmail.com>
2015-11-17 22:54:33 +0100

    collectable type enum added

--- Classes/Collectable.cpp
+++ Classes/Collectable.cpp
@@ -10,8 +10,8 @@ Collectable::Collectable(void){
 void Collectable::spawn(cocos2d::Layer* layer, std::vector<Sprite*>& collectables, int pattern) {
     if (not layer) { return; }
     
-    const float distanceBetweenEggs = 0.001;
-    int numberOfEggs = 0;
+    const float distanceBetweenCollectables = 0.001;
+    int numberOfCollectable = 0;
     int degree = 0;
     float radius = 0.0;
     
@@ -19,17 +19,17 @@ void Collectable::spawn(cocos2d::Layer* layer, std::vector<Sprite*>& collectable
         case 0:
             return;
         case 1:
-            numberOfEggs = 5;
+            numberOfCollectable = 5;
             degree = 45;
             radius = 0.075;
             break;
         case 2:
-            numberOfEggs = 7;
+            numberOfCollectable = 7;
             degree = 30;
             radius = 0.15;
             break;
         case 3:
-            numberOfEggs = 3;
+            numberOfCollectable = 3;
             degree = 90;
             radius = 0.05;
             break;
@@ -38,10 +38,25 @@ void Collectable::spawn(cocos2d::Layer* layer, std::vector<Sprite*>& collectable
             return;
     }
     
-    int eggType = CCRANDOM_0_1() * 4 + 1;
-//    if (eggType == 3) {
-//        eggType = 1;  // handle boundary condition
-//    }
+    int collectableType = CCRANDOM_0_1() * 4 + 1;
+    switch (collectableType) {
+        case 1:
+            _type = CollectableType::egg;
+            break;
+        case 2:
+            _type = CollectableType::egg;
+            break;
+        case 3:
+            _type = CollectableType::bomb;
+            break;
+        case 4:
+            _type = CollectableType::pizza;
+            break;
+            
+        default:
+            _type = CollectableType::egg;
+            break;
+    }
     
     int heightRange = _visibleSize.height * 0.25;
     int minHeight = (_visibleSize.height * 0.4);
@@ -49,16 +64,16 @@ void Collectable::spawn(cocos2d::Layer* layer, std::vector<Sprite*>& collectable
     
     float positionX = _visibleSize.width;
     float positionY;
-    for (int i = 0; i < numberOfEggs; ++i) {
-        Sprite* collectable = Sprite::create(String::createWithFormat("egg%i.png", eggType)->getCString());
+    for (int i = 0; i < numberOfCollectable; ++i) {
+        Sprite* collectable = Sprite::create(String::createWithFormat("egg%i.png", collectableType)->getCString());
         
-        positionX += _visibleSize.width * distanceBetweenEggs; // distance between eggs
+        positionX += _visibleSize.width * distanceBetweenCollectables; // distance between collectables
         positionY = (_visibleSize.width * radius) * sin(degree2radian(i * degree)); // y = radius * sin(angle) // bigger radius = higher parabola
         collectable->setPosition(Vec2(positionX, minPosY + positionY));
         
         auto body = PhysicsBody::createCircle(collectable->getContentSize().width / 2, PhysicsMaterial(0.1f, 1.0f, 0.0f));
-        body->setCategoryBitmask(CATEGORY_BITMASK_EGG);
-        // eggBody->setCollisionBitmask(1);
+        body->setCategoryBitmask(CATEGORY_BITMASK_COLLECTABLE);
+        // body->setCollisionBitmask(1);
         body->setContactTestBitmask(CATEGORY_BITMASK_CHICKEN);
         body->setDynamic(false);
         collectable->setPhysicsBody(body);
@@ -66,7 +81,7 @@ void Collectable::spawn(cocos2d::Layer* layer, std::vector<Sprite*>& collectable
         layer->addChild(collectable, BackgroundLayer::layerChicken);
         collectables.push_back(collectable);
         
-        positionX += collectable->getContentSize().width * 1.5; // distance must be atleast 1.5 egg width
+        positionX += collectable->getContentSize().width * 1.5; // distance must be atleast 1.5 collectable width
     }
 }
 
--- Classes/Collectable.h
+++ Classes/Collectable.h
@@ -1,9 +1,17 @@
-#ifndef __EGG_SPRITE__
-#define __EGG_SPRITE__
+#ifndef __COLLECTABLE_SPRITE__
+#define __COLLECTABLE_SPRITE__
 
 #include <cocos2d.h>
 
 using namespace cocos2d;
+
+typedef enum {
+    egg,
+    pizza,
+    bomb
+    
+} CollectableType;
+
 
 class Collectable {
 public:
@@ -14,9 +22,10 @@ private:
     Vec2 _origin;
     Size _visibleSize;
     Sprite* _collectable;
+    CollectableType _type;
     
     std::string const _imageFile = "egg1.png";
 };
 
 
-#endif /* __EGG_SPRITE__ */
+#endif /* __COLLECTABLE_SPRITE__ */
