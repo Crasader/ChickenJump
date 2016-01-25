6b8293dcbb7b5671ce0c37e1553ad0b40c87fa98
asish012 <asish012@gmail.com>
2016-01-25 20:30:47 +0100

    unlocked all stages (for testing)

--- Classes/StageStatus.cpp
+++ Classes/StageStatus.cpp
@@ -14,13 +14,13 @@ const std::string StageStatus::netherlands = "NL";
 const std::string StageStatus::infinite = "INFINITE";
 
 void StageStatus::createFreshStages() {
-    // name, imagefile, score, star, isunlocked, isPlayed
+    // name, imagefile, clicked_image, locked_image, difficulty, score, star, isunlocked, isPlayed
     saveStage(Stage(france, "FR.png", "FR_clicked.png", "FR_locked.png", 1, 0, 0, true, false));
-    saveStage(Stage(italy, "IT.png", "IT_clicked.png", "IT_locked.png", 2, 0, 0, false, false));
-    saveStage(Stage(england, "UK.png", "UK_clicked.png", "UK_locked.png", 3, 0, 0, false, false));
-    saveStage(Stage(germany, "DE.png", "DE_clicked.png", "DE_locked.png", 4, 0, 0, false, false));
-    saveStage(Stage(spain, "SP.png", "SP_clicked.png", "SP_locked.png", 5, 0, 0, false, false));
-    saveStage(Stage(netherlands, "NL.png", "NL_clicked.png", "NL_locked.png", 6, 0, 0, false, false));
+    saveStage(Stage(italy, "IT.png", "IT_clicked.png", "IT_locked.png", 2, 0, 0, true, false));
+    saveStage(Stage(england, "UK.png", "UK_clicked.png", "UK_locked.png", 3, 0, 0, true, false));
+    saveStage(Stage(germany, "DE.png", "DE_clicked.png", "DE_locked.png", 4, 0, 0, true, false));
+    saveStage(Stage(spain, "SP.png", "SP_clicked.png", "SP_locked.png", 5, 0, 0, true, false));
+    saveStage(Stage(netherlands, "NL.png", "NL_clicked.png", "NL_locked.png", 6, 0, 0, true, false));
     saveStage(Stage(infinite, "INFINITE.png", "INFINITE_clicked.png", "INFINITE_locked.png", 7, 0, 0, true, false));
 }
 
