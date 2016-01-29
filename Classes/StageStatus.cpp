343051df3de906918ff41323032bcebef15f484f
asish012 <asish012@gmail.com>
2016-01-29 21:03:13 +0100

    stage number re-ordered.

--- Classes/StageStatus.cpp
+++ Classes/StageStatus.cpp
@@ -15,21 +15,21 @@ const std::string StageStatus::infinite = "INFINITE";
 
 void StageStatus::createFreshStages() {
     // name, imagefile, clicked_image, locked_image, difficulty, score, highScore, star, isunlocked, isPlayed
-    saveStage(Stage(france, "FR.png", "FR_clicked.png", "FR_locked.png", 1, 0, 0, 0, true, false));
-    saveStage(Stage(italy, "IT.png", "IT_clicked.png", "IT_locked.png", 2, 0, 0, 0, true, false));
-    saveStage(Stage(england, "UK.png", "UK_clicked.png", "UK_locked.png", 3, 0, 0, 0, true, false));
-    saveStage(Stage(germany, "DE.png", "DE_clicked.png", "DE_locked.png", 4, 0, 0, 0, true, false));
-    saveStage(Stage(spain, "SP.png", "SP_clicked.png", "SP_locked.png", 5, 0, 0, 0, true, false));
+    saveStage(Stage(england, "UK.png", "UK_clicked.png", "UK_locked.png",     1, 0, 0, 0, true, false));
+    saveStage(Stage(italy, "IT.png", "IT_clicked.png", "IT_locked.png",       2, 0, 0, 0, true, false));
+    saveStage(Stage(france, "FR.png", "FR_clicked.png", "FR_locked.png",      3, 0, 0, 0, true, false));
+    saveStage(Stage(germany, "DE.png", "DE_clicked.png", "DE_locked.png",     4, 0, 0, 0, true, false));
+    saveStage(Stage(spain, "SP.png", "SP_clicked.png", "SP_locked.png",       5, 0, 0, 0, true, false));
     saveStage(Stage(netherlands, "NL.png", "NL_clicked.png", "NL_locked.png", 6, 0, 0, 0, true, false));
     saveStage(Stage(infinite, "INFINITE.png", "INFINITE_clicked.png", "INFINITE_locked.png", 7, 0, 0, 0, true, false));
 }
 
 std::vector<Stage> StageStatus::getStage(void) {
     std::vector<Stage> stages;
-    stages.push_back(getStage(france));
-    stages.push_back(getStage(italy));
-    stages.push_back(getStage(germany));
     stages.push_back(getStage(england));
+    stages.push_back(getStage(italy));
+    stages.push_back(getStage(france));
+    stages.push_back(getStage(germany));
     stages.push_back(getStage(spain));
     stages.push_back(getStage(netherlands));
     stages.push_back(getStage(infinite));
