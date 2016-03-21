f5a2c8997d4fb70255448f9e78b6b9bc834d6db3
asish012 <asish012@gmail.com>
2016-03-21 22:33:30 +0100

    Fullscreen mode activated

--- proj.android/src/sonar/systems/framework/SonarFrameworkActivity.java
+++ proj.android/src/sonar/systems/framework/SonarFrameworkActivity.java
@@ -1,6 +1,7 @@
 package sonar.systems.framework;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ public class SonarFrameworkActivity extends Cocos2dxActivity
 {
 	
 	private static SonarFrameworkFunctions functions = null;
+	private Cocos2dxGLSurfaceView glSurfaceView;
 	
 	protected SonarFrameworkActivity()
 	{
@@ -28,6 +30,46 @@ public class SonarFrameworkActivity extends Cocos2dxActivity
 		}
 		functions.onCreate(b);
 	}
+	
+	//
+    // Fullscreen Mode Begin
+	//
+	public Cocos2dxGLSurfaceView onCreateView()
+    {
+        glSurfaceView = new Cocos2dxGLSurfaceView(this);
+
+        this.hideSystemUI();
+
+        // create stencil buffer
+        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
+
+        return glSurfaceView;
+    }
+    
+
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus)
+        {
+            this.hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI()
+    {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        glSurfaceView.setSystemUiVisibility(
+                Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_STABLE 
+                | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | Cocos2dxGLSurfaceView.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+    // Fullscreen Mode End
 
 	@Override
 	protected void onStart() 
